f3c7804c96ab887acb4a66a4ba7cd8ed3faa8d2c
Andrey Epifanov <aepifanov@viacode.com>
2020-02-04 16:54:15 +0300

    Added alert search fields

---
+++ db/migrate/20200402134700_add_alert_search_fields.rb
@@ -0,0 +1,12 @@
+class AddAlertSearchFields < ActiveRecord::Migration[5.1]
+  def up
+
+    # return if it's a new setup
+    return if !Setting.find_by(name: 'system_init_done')
+
+    add_column :tickets, :alert_name, :string, null: true
+    add_column :tickets, :alert_target, :string, null: true
+    add_column :tickets, :alert_monitoring_service, :string, null: true
+    
+  end
+end
