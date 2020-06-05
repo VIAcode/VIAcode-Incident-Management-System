f20713e421cc87f213c0874e879a56767154c83c
Andrey Epifanov <aepifanov@viacode.com>
2020-06-05 20:50:59 +0300

    Fixed typo

--- app/models/observer/ticket/article/communicate_azure_dev_ops.rb
+++ app/models/observer/ticket/article/communicate_azure_dev_ops.rb
@@ -9,7 +9,7 @@ class Observer::Ticket::Article::CommunicateAzureDevOps < ActiveRecord::Observer
     if ticket.external_ticket_id
       user = User.find_by(id: record.created_by_id)    
       if user.login != 'azdevops'
-        host = Settings.get('fqdn')
+        host = Setting.get('fqdn')
         org = ticket.organization
         Net::HTTP.post_form URI('https://' + host.split(".").first + '-azdevops' + host[host.index('.')..-1] + '/vo-api/ArticleAdded'), { "workitemid" => ticket.external_ticket_id, "body" => record.body, "author" => user.firstname + " " + user.lastname, "ticketlink" => "https://" + host + "/#ticket/zoom/#{ticket.id}", "ticketnumber" => ticket.number, "organization" => org.azuredevops_organization, "project" => org.azuredevops_project, "area" => org.azuredevops_area, "token" => org.azuredevops_token }
       end
