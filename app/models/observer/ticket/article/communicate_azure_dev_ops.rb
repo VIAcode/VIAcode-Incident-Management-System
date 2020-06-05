c617d2ff92936852fc3e1976634f98d15a61eba7
Andrey Epifanov <aepifanov@viacode.com>
2020-06-05 19:44:59 +0300

    Added author and link to ticket to article notification

--- app/models/observer/ticket/article/communicate_azure_dev_ops.rb
+++ app/models/observer/ticket/article/communicate_azure_dev_ops.rb
@@ -9,9 +9,9 @@ class Observer::Ticket::Article::CommunicateAzureDevOps < ActiveRecord::Observer
     if ticket.external_ticket_id
       user = User.find_by(id: record.created_by_id)    
       if user.login != 'azdevops'
-        host = ENV["HOST_NAME"]
+        host = Settings.get('fqdn')
         org = ticket.organization
-        Net::HTTP.post_form URI('https://' + host.split(".").first + '-azdevops' + host[host.index('.')..-1] + '/vo-api/ArticleAdded'), { "workitemid" => ticket.external_ticket_id, "body" => record.body, "organization" => org.azuredevops_organization, "project" => org.azuredevops_project, "area" => org.azuredevops_area, "token" => org.azuredevops_token }
+        Net::HTTP.post_form URI('https://' + host.split(".").first + '-azdevops' + host[host.index('.')..-1] + '/vo-api/ArticleAdded'), { "workitemid" => ticket.external_ticket_id, "body" => record.body, "author" => user.firstname + " " + user.lastname, "ticketlink" => "https://" + host + "/#ticket/zoom/#{ticket.id}", "ticketnumber" => ticket.number, "organization" => org.azuredevops_organization, "project" => org.azuredevops_project, "area" => org.azuredevops_area, "token" => org.azuredevops_token }
       end
     end
   end
